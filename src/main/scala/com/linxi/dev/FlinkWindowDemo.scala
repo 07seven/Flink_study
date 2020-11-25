1d1bb59fe590c4bfb5cac244053860ad30870158
linxi <weixi96007@163.com>
2020-11-25 14:40:35 +0800

    commit

--- src/main/scala/com/linxi/dev/FlinkWindowDemo.scala
+++ src/main/scala/com/linxi/dev/FlinkWindowDemo.scala
@@ -1,8 +1,9 @@
 package com.linxi.dev
 
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
-import org.apache.flink.table.api.{EnvironmentSettings, Table}
+import org.apache.flink.table.api.{EnvironmentSettings, GroupWindow, Table, Tumble}
 import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.types.Row
 
 object FlinkWindowDemo {
   def main(args: Array[String]): Unit = {
@@ -20,10 +21,17 @@ object FlinkWindowDemo {
 
     val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env, settings)
 
-    //val table: Any = tableEnv.fromDataStream(input, 'id, 'timestamp, 'temperature)(
-
-    val sensorTable: Table = tableEnv.fromDataStream(input)
+    //需要导入隐式转换
+    import org.apache.flink.table.api.scala._
+    import org.apache.flink.api.scala._
+    val sensorTable: Table = tableEnv.fromDataStream(input, 'id, 'timestamp as 'ts, 'temperature)
+    //val sensorTable: Table = tableEnv.fromDataStream(input)
     //相关window操作
+    val windowTable: Table = sensorTable.window(Tumble over 10.minutes on 'ts as 'w)
+      .groupBy('id, 'w)
+      .select('id, 'id.count)
+
+    windowTable.toAppendStream[Row].print()
 
   }
 
